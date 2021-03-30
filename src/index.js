655480eecbc768fe9849771a885314d5b40ceddf
Danloukusa <Daniel.Loukusa@hotmail.com>
2021-03-30 13:09:56 -0400

    make home list appear by default, change how it looks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Create factory for ToDo ITEM
 // import toDoItem from './toDoItem';
 import toDoClass from './toDoItem';
-    function decideList(){
+
+    function showSubLists(){
         let x;
+        newBackground(this);
         for(x in listArray)
         {
             if (listArray[x].parentProject == this.id)
@@ -10,11 +12,56 @@ import toDoClass from './toDoItem';
         }
     }
 
+    let prev = "ooga";
+
+    function newBackground(loc)
+    {
+        if(prev == "ooga")
+        {
+            loc.style.backgroundColor = "blue";
+            prev = loc;
+        }
+        else{
+            prev.style.backgroundColor = "pink";
+            prev = loc;
+            loc.style.backgroundColor = "blue";
+        }
+
+    }
+
+
     function showAllLists(array){
         let item;
+
+        let header = document.createElement("div");
+        header.id = "buttons";
+        document.getElementById("megaContainer").appendChild(header);
+    
+        header = document.createElement("button");
+        header.id = "mainButton";
+        header.innerText = "Show Main List";
+        document.getElementById("buttons").appendChild(header);
+
+        header = document.createElement("button");
+        header.innerText = "New List";
+        header.id = "newList";
+        header.style.display = "inline";
+        document.getElementById("buttons").appendChild(header);
+
+        header = document.createElement("button");
+        header.innerText = "Edit Current List";
+        header.id = "editList";
+        header.style.display = "inline";
+        document.getElementById("buttons").appendChild(header);
+
+        let title = document.createElement("label");
+        title.id = "currentListTitle";
+        title.innerText = "Category Lists";
+        title.style.display = "block";
+        document.getElementById("megaContainer").appendChild(title);
+
         let mainList = document.createElement("ul");
         mainList.id = "mainList";
-        mainList.innerText = "MAIN LIST";
         document.getElementById("megaContainer").appendChild(mainList);
 
         for(item in array){
@@ -23,14 +70,30 @@ import toDoClass from './toDoItem';
                 let list = document.createElement("li");
                 list.id = array[item].parentProject;
                 list.innerText = list.id;
-                list.addEventListener("click", decideList);
+                list.addEventListener("click", showSubLists);
                 console.log(list.id);
                 document.getElementById("mainList").appendChild(list);
             }
         }
     }
+
+    function clearSubLists(){
+        
+        let mainParent = document.getElementById("megaContainer");
+        let num = 0;
+        for(num = 0; num < 4; num++)
+        {
+            if(mainParent.childNodes.length > 5)
+                mainParent.removeChild(mainParent.lastChild);
+
+        }
+        document.getElementById("makeList").style.display = "none";
+    }
+
     function showAList(toDoObject){
+        clearSubLists();
         let containerExist = doesContainerExist(toDoObject.listName);
+        // alert(containerExist);
         if(!containerExist)
         {
             let container = document.createElement("ol");
@@ -38,9 +101,14 @@ import toDoClass from './toDoItem';
             container.className = toDoObject.parentProject;
             container.innerText = container.id;
             document.getElementById("megaContainer").appendChild(container);
+            showHelper(toDoObject.list, toDoObject.listName);
+        }
+        else
+        {
+            // List REFRESH functionality would be nice
         }
 
-        showHelper(toDoObject.list, toDoObject.listName);
+        // showHelper(toDoObject.list, toDoObject.listName);
         // This will take an ENTIRE list, and show it in a box on the page.
         // What does a list look like?
 
@@ -104,6 +172,13 @@ function main(){
     t.addToList("Realize forgotton JumpRope")
     t.parentProject = "shoeStuff";
 
+    let x = toDoClass("Gaming");
+    storeList(x);
+    x.addToList("Get a PC and peripherals");
+    x.addToList("Install games");
+    x.addToList("Play Games");
+    x.parentProject = "Entertainment";
+
     //showAList(newThing);
     //showAList(secondList);
     // console.log(newThing.list);
@@ -114,9 +189,9 @@ function main(){
     // LOCAL STORAGE SECTION **********************
     // ********************************************
     
-    //let JSONREADYarray = JSON.stringify(listArray);
+    let JSONREADYarray = JSON.stringify(listArray);
 
-    //ocalStorage.setItem("ToDoList Collection", JSONREADYarray);
+    localStorage.setItem("ToDoList Collection", JSONREADYarray);
     console.log(JSON.parse(localStorage['ToDoList Collection']));
 
     listArray = JSON.parse(localStorage['ToDoList Collection']);
@@ -124,12 +199,15 @@ function main(){
     // console.log(JSON.parse(localStorage['ToDoList Collection']));
 
     // What should I present it as on the DOM?
+
+    showAllLists(JSON.parse(localStorage['ToDoList Collection']));
 };
 main();
 
 function overlayListener(){
-    document.getElementById("newList").addEventListener("click", removeOverlay);
-    document.getElementById("viewAll").addEventListener("click", removeOverlay);
+    document.getElementById("newList").addEventListener("click", listCreator);
+    // document.getElementById("viewAll").addEventListener("click", removeOverlay);
+    document.getElementById("mainButton").addEventListener("click", clearSubLists);
     document.getElementById("radioDefault").addEventListener("click", hideCustomInput);
     document.getElementById("radioCustom").addEventListener("click", showCustomInput);
 }
@@ -146,16 +224,23 @@ function hideCustomInput(){
 
 overlayListener();
 
-function removeOverlay(){
-    if(this.id == "newList")
-    {
-        document.getElementById("makeList").style.display = "inline";
-    }
-    if(this.id == "viewAll")
-    {
-        showAllLists(JSON.parse(localStorage['ToDoList Collection']));
-        // include in the form, parent project section.
-    }
-
-    document.getElementById("overlay").style.display = "none";
+function listCreator(){
+    document.getElementById("makeList").style.display = "inline";
 }
+
+/*
+    function removeOverlay(){
+        if(this.id == "newList")
+        {
+            document.getElementById("makeList").style.display = "inline";
+        }
+        if(this.id == "viewAll")
+        {
+            
+            showAllLists(JSON.parse(localStorage['ToDoList Collection']));
+            // include in the form, parent project section.
+        }
+
+        document.getElementById("overlay").style.display = "none";
+    }
+*/
